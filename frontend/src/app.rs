6c5ff9a1ff80e71d30c9661409dac824dee0ef57
SziluChannel <szaszilumin@gmail.com>
2023-01-29 17:03:04 +0100

    web module is working with demo

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,35 +1,42 @@
+use wasm_bindgen::JsCast;
 use yew::prelude::*;
 use crate::modules::requests;
+use web_sys::HtmlInputElement;
 use crate::BACKEND;
 
 #[function_component]
 pub fn App() -> Html {
 
-    let text = use_state(|| String::from("Hello"));
-    let phrase = use_state(|| String::new());
+    let text = use_state(|| String::new());
+    let answer = use_state(|| String::new());
 
     let onclick = {
-        let phrase = phrase.clone();
+        let answer = answer.clone();
         let text = text.clone();
-
-        move |_event: MouseEvent| {
-        let phrase = phrase.clone();
-        let text = text.clone();
+        move |_: MouseEvent| {
+            let answer = answer.clone();
+            let text = text.clone();
             wasm_bindgen_futures::spawn_local(async move {
             let tmp = requests::post_request::<String>(
                 &format!("{BACKEND}/echo"),
                 String::from(&*text))
                 .await
                 .expect("Error with the request!!");
-            phrase.set(tmp.to_string())
+                answer.set(tmp.to_string())
             })
         }
     };
 
+    let on_text_input = Callback::from(move |event: Event| {
+            text.set(event.target().unwrap().unchecked_into::<HtmlInputElement>().value());
+        });
+
     html! {
         <div>
+            <h1>{"Add some text for testing purposes:"}<br/></h1>
+            <input type={"text"} onchange={on_text_input} />
             <button {onclick}>{ "Send!" }</button>
-            <p>{"Answer: "}{ &*phrase }</p>
+            <p>{"Answer: "}{ &*answer }</p>
         </div>
     }
 }
